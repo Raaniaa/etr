5a55755188c988add704e12be5901833b921b09b
Raaniaa <rania.rony76@gmail.com>
2021-10-19 10:49:00 +0200

    product

---
+++ database/migrations/2021_10_19_081510_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->string('price')->nullable();
+            $table->string('discount')->nullable();
+            $table->string('discription')->nullable();
+            $table->boolean('isOffer')->nullable()->default(false);
+            $table->bigInteger('boutique_id')->nullable()->default(12)->unsigned();
+            $table->foreign('boutique_id')->references('id')->on('boutiques')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
